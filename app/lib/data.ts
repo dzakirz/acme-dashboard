4c948c6691894873aa9ede3915476ba6649e5465
dzakirz <dzakirz.dev@gmail.com>
2024-01-30 14:05:19 +0700

    add streaming

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,6 +37,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest invoices...');
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
@@ -48,6 +51,9 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    console.log('Data fetch completed after 3 seconds.');
+
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
