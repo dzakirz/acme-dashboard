7e1b5de493be165c8a782aef7e91745820d51ca6
dzakirz <dzakirz.dev@gmail.com>
2024-01-30 14:54:59 +0700

    create search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,5 +1,4 @@
-import { fetchCardData } from '@/app/lib/data';
-import CardWrapper, { Card } from '@/app/ui/dashboard/cards';
+import CardWrapper from '@/app/ui/dashboard/cards';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import { lusitana } from '@/app/ui/fonts';
@@ -11,13 +10,6 @@ import {
 import { Suspense } from 'react';
 
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData(); // wait for fetchLatestInvoices() to finish
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
